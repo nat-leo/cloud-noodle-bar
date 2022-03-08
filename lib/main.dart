39933ee81c005b6ec0ea573be6c46a0e032d8a08
natleo <n8wyliu@gmail.com>
2022-03-07 18:13:26 -0800

    Add 1/2 Landing Page UI: Call to action and value proposition. Still missing social proof and visual

--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,77 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Landing Page',
-        ),
+        child: HeroText(),
       ),
     );
   }
 }
+
+class HeroText extends StatelessWidget {
+  const HeroText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          const Text(
+            "Ramen-ya",
+            style: TextStyle(
+              color: Color(0xFF484747),
+              fontSize: 60,
+            ),
+          ),
+          const Divider(
+            color: Color(0xFF484747),
+            thickness: 5,
+            indent: 50,
+            endIndent: 50,
+          ),
+          const Text(
+            "Ramen Shop",
+            style: TextStyle(
+              color: Color(0xFF484747),
+              fontSize: 60,
+            ),
+          ),
+          const Text(
+            "in a Cup.",
+            style: TextStyle(
+              color: Color(0xFF484747),
+              fontSize: 60,
+            ),
+          ),
+          ElevatedButton(
+              style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFF484747),
+                minimumSize: const Size(331, 81),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(100))
+                )
+              ),
+              onPressed: (){},
+              child: const Text(
+                "Order now",
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 30,
+                ),
+              ))
+        ],
+      )
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image(image: AssetImage('resources/hero-background.jpg')),
+    );
+  }
+}
+
